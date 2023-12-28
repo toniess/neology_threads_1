fb3a088affdea607afdb2a13ffc8015a90ece9e0
Ignatiy Glushkov <64589798+toniess@users.noreply.github.com>
2023-12-28 19:18:25 +0300

    Add files via upload

--- threads_1_2/main.cpp
+++ threads_1_2/main.cpp
@@ -3,8 +3,9 @@
 #include <thread>
 #include <chrono>
 #include <numeric>
+#include <iomanip>
 
-void parallelVectorSum(const std::vector<int>& v1, const std::vector<int>& v2, int numThreads) {
+double parallelVectorSum(const std::vector<int>& v1, const std::vector<int>& v2, int numThreads) {
     std::vector<std::thread> threads(numThreads);
     std::vector<int> result(v1.size());
 
@@ -32,26 +33,32 @@ void parallelVectorSum(const std::vector<int>& v1, const std::vector<int>& v2, i
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
 
-    std::cout << "Время занятое " << numThreads << " потоками: " << elapsed.count() << " секунд" << std::endl;
+    return elapsed.count();
 }
-
 
 int main() {
     std::vector<int> arraySizes = {1000, 10000, 100000, 1000000};
     std::vector<int> hardwareThreads = {2, 4, 8, 16};
 
-    std::cout << "Количество аппаратных ядер: " << std::thread::hardware_concurrency() << "\n\n";
-
-    for (auto size : arraySizes) {
-        std::vector<int> vec1(size, 1);
-        std::vector<int> vec2(size, 2);
-
-        std::cout << "Размер массива: " << size << std::endl;
+    std::cout << "Количество аппаратных ядер: " << std::thread::hardware_concurrency() << std::endl;
 
 
-        for (int threads : hardwareThreads)
-            parallelVectorSum(vec1, vec2, threads);
+    std::cout << std::left << std::setw(15) << " ";
+    for (int size : arraySizes)
+        std::cout << std::left << std::setw(15) << size;
+    std::cout << "\n";
 
+
+    for (int threads : hardwareThreads) {
+        std::cout << std::left << std::setw(20) << std::to_string(threads) + " потоков ";
+
+        for (auto size : arraySizes) {
+            std::vector<int> vec1(size, 1);
+            std::vector<int> vec2(size, 2);
+
+            std::cout << std::left << std::setw(15)
+                      << parallelVectorSum(vec1, vec2, threads);
+        }
         std::cout << std::endl;
     }
 
